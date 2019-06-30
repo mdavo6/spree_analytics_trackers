be4af3c4e77875e385be1de081e398763dd21736
Michael Davidson <mdavo6@gmail.com>
2019-06-30 18:53:33 +1000

    Remove duplicate GA script

--- app/overrides/add_google_analytics_to_spree_application.rb
+++ app/overrides/add_google_analytics_to_spree_application.rb
@@ -1,9 +1,6 @@
-if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
-  Deface::Override.new(
-    virtual_path: 'spree/layouts/spree_application',
-    name: 'add_google_analytics_to_spree_application',
-    insert_top: "[data-hook='body']",
-    partial: 'spree/shared/google_analytics',
-    original: 'cfa30a2831d9a41394c03229cd28b3c7eee69585'
-  )
-end
+Deface::Override.new(
+  virtual_path: 'spree/layouts/spree_application',
+  name: 'add_google_analytics_to_spree_application',
+  insert_bottom: "[data-hook='inside_head']",
+  partial: 'spree/shared/google_analytics'
+)
