05fe85b65f8ae4788e9354367b39ed915a53d9c7
Michael Davidson <mdavo6@gmail.com>
2020-06-10 18:48:31 +1000

    Move GA to top of body and add custom wholesale dimension

--- app/overrides/add_google_analytics_to_spree_application.rb
+++ app/overrides/add_google_analytics_to_spree_application.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   virtual_path: 'spree/layouts/spree_application',
   name: 'add_google_analytics_to_spree_application',
-  insert_bottom: "[data-hook='inside_head']",
+  insert_top: '[data-hook="body"]',
   partial: 'spree/shared/google_analytics',
   original: '36e1ead6988d71decab562f4ae0a2d503da3d885'
 )
